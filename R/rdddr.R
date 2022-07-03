431c5cdceae45a1cddfcde5a9d72beb64e12374c
Graeme Blair <graeme.blair@gmail.com>
2022-07-03 08:20:02 +0000

    version 1.0.0

---
+++ R/rdddr.R
@@ -0,0 +1,14 @@
+#' rdddr package
+#'
+#' Companion datasets and functions for the book "Research Design: Declare, Diagnose, Redesign" (book.declaredesign.org)
+#'
+#' @docType package
+#' @name rdddr
+NULL
+
+#' @importFrom generics tidy
+#' @export
+generics::tidy
+
+utils::globalVariables(c("X", "variance", "estimate", "std.error", "value", "name", "order_by___", "state", "fitted", "PS_weight", "covariates", "pred", "type", "tau.squared", "tau.squared.se", "term", "ci.lb", "ci.ub", "Estimate", "Std. Err", "z value", "Pr(>|z|)", "Attribute", "Level", "profile", "attribute", "level", "partial", "conjoint_utility", "subject", "task", "U", "XY"))
+
